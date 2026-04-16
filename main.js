d96825d0e24b85b61cf3dfc1d6fbbfdcbf61003f
akthernahela <nahela00@gmail.com>
2026-04-16 15:42:46 +0200

    Snack 1 done

--- main.js
+++ main.js
@@ -7,7 +7,19 @@ e restituisce una Promise che recupera il titolo
 di un post dal link https://dummyjson.com/posts/{id}
 */
 
+const getPostTitle = id => {
+    const promessa = new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(obj => resolve(obj))
+            .catch(reject);
+    })
+    return promessa;
+}
 
+getPostTitle(1)
+    .then(obj => console.log(obj))
+    .catch(error => console.error(error))
 //Snack 2
 
 /*
