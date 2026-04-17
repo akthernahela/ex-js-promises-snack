426dd9578702b2c1dca995dd817975515a4a472b
akthernahela <nahela00@gmail.com>
2026-04-17 15:23:09 +0200

    Set the function

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ Crea una funzione getPostTitle(id) che accetta un id
 e restituisce una Promise che recupera il titolo
 di un post dal link https://dummyjson.com/posts/{id}
 */
-
+/*
 const getPostTitle = id => {
     const promessa = new Promise((resolve, reject) => {
         fetch(`https://dummyjson.com/posts/${id}`)
@@ -20,6 +20,7 @@ const getPostTitle = id => {
 getPostTitle(1)
     .then(obj => console.log(obj))
     .catch(error => console.error(error))
+*/
 //Snack 2
 
 /*
@@ -27,3 +28,16 @@ Crea la funzione lanciaDado() che restituisce una Promise che,
 dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, 
 nel 20 % dei casi, il dado si "incastra" e la Promise va in reject.
 */
+
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() < 0.2) {
+                reject('Il dado si è incastrato!')
+            } else {
+                const numero = Math.floor(Math.random() * 6) + 1;
+                resolve(numero);
+            }
+        }, 3000);
+    });
+}
